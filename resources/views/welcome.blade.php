5c7e17bc1929da67e69063d4a0a8eba1466eff11
Vasili Raminashvili <vaska.r02@gmail.com>
2024-12-17 00:05:11 +0400

    wip on main paige

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-layout.master>
-    hey
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae cum ea eius excepturi fugit ipsa ipsam
+    perspiciatis similique soluta? Aspernatur assumenda consectetur facilis mollitia possimus ratione, repellendus
+    repudiandae vitae?
 </x-layout.master>
